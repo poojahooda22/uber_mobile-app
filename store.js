f6149cad356b6564bb7aa21d05a60ac367197f7b
pooja <91055527+poojahooda22@users.noreply.github.com>
2023-11-09 17:50:49 +0530

    update store

--- store.js
+++ store.js
@@ -1,4 +1,5 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { configureStore } from '@reduxjs/toolkit';
+import 
 import rootReducer from './reducers'
 
 const store = configureStore({
