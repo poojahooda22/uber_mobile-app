daf54294b9e39faba1b52e86e3ff6f6ed03e52dc
pooja <91055527+poojahooda22@users.noreply.github.com>
2023-11-09 17:49:42 +0530

    update app

--- App.js
+++ App.js
@@ -1,12 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { Provider } from 'react-redux';
+
+//set up redux
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Let's build Uber!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Provider>
+      <View style={styles.container}>
+        <Text>Let's build UBER!</Text>
+        <StatusBar style="auto" />
+      </View>
+    </Provider>
   );
 }
 
