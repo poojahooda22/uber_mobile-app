7f9836c88de2fe99b983e947656271121d6db67d
pooja <91055527+poojahooda22@users.noreply.github.com>
2023-11-10 16:59:20 +0530

    updated

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -8,14 +8,14 @@ const HomeScreen = () => {
     <SafeAreaView style={tw `bg-white h-full`}>
       <View style={tw `p-5`}>
         <Image
-            style={{
-                width: 100, 
-                height: 100,
-                resizeMode: 'contain',
-            }}
-            source={{
-                uri: "https://links.papareact.com/gzs"
-            }}
+          style={{
+            width: 100, 
+            height: 100,
+            resizeMode: 'contain',
+          }}
+          source={{
+            uri: "https://links.papareact.com/gzs"
+          }}
         />
         <NavOptions />
       </View>
