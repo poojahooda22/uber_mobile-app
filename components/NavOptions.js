3c7b5fbc9b594e5155a6a53b5fa570d745e936af
pooja <91055527+poojahooda22@users.noreply.github.com>
2023-11-10 14:30:42 +0530

    create navigation

---
+++ components/NavOptions.js
@@ -0,0 +1,57 @@
+import { FlatList, StyleSheet, Text, TouchableOpacity, View, Image } from 'react-native'
+import React from 'react'
+import tw from 'twrnc'
+import { Icon } from 'react-native-elements';
+import { useNavigation } from '@react-navigation/native';
+
+const data = [
+    { 
+        id: "123",
+        title: "Gte a ride",
+        image: "https://links.papareact.com/3pn",
+        screen: "MapScreen",
+    },
+    { 
+        id: "456",
+        title: "Order food",
+        image: "https://links.papareact.com/28w",
+        screen: "EatsScreen",
+    },
+];
+
+
+const NavOptions = () => {
+    const navigation = useNavigation();
+  return (
+    <FlatList
+        data={data}
+        horizontal
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => (
+            <TouchableOpacity 
+                style={tw `p-2 pl-6 pb-8 pt-4 bg-gray-200 m-2 w-40`}>
+                    onPress={() => navigation.navigate(item.screen)}
+               <View>
+                <Image
+                    source={{ uri: item.image }}
+                    style={{ width: 120, height: 120, resizeMode: 'contain'}}
+                />
+                <Text style={tw `font-semibold mt-2 text-lg`}>
+                    {item.title}
+                </Text>
+                <Icon
+                    style={tw `p-2 bg-black rounded-full w-10 mt-4`}
+                    name="arrowright"
+                    color="white"
+                    type="antdesign"
+                />
+               </View>
+            </TouchableOpacity>
+        )}
+    />
+  )
+}
+
+export default NavOptions
+
+const styles = StyleSheet.create({})
