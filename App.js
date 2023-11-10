82c8683a7c237c07a55fd3a78f4ba68d0861d1a8
pooja <91055527+poojahooda22@users.noreply.github.com>
2023-11-10 14:30:31 +0530

    app updated

--- App.js
+++ App.js
@@ -1,16 +1,40 @@
-import { StatusBar } from 'expo-status-bar';
+
 import { StyleSheet, Text, View } from 'react-native';
 import { Provider } from 'react-redux';
+import { store } from './store';
+import HomeScreen from './screens/HomeScreen';
+import MapScreen from './screens/MapScreen';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import 'react-native-gesture-handler';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
 //set up redux
 
 export default function App() {
   return (
-    <Provider>
-      <View style={styles.container}>
-        <Text>Let's build UBER!</Text>
-        <StatusBar style="auto" />
-      </View>
+    <Provider store={store}>
+      <NavigationContainer>
+        <SafeAreaProvider>
+          <Stack.Navigator>
+            <Stack.Screen
+              name="HomeScreen"
+              component={HomeScreen}
+              options={{
+                headerShown: false,
+              }}
+            />
+            <Stack.Screen
+              name="MapScreen"
+              component={MapScreen}
+              options={{
+                headerShown: false,
+              }}
+            />
+          </Stack.Navigator>
+          {/* <HomeScreen /> */}
+        </SafeAreaProvider>
+      </NavigationContainer>      
     </Provider>
   );
 }
