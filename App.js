f1cb7ab9741ae6ecc2e8893069ecfc6d2fc96f3f
pooja <91055527+poojahooda22@users.noreply.github.com>
2023-11-10 15:14:20 +0530

    app updated

--- App.js
+++ App.js
@@ -33,7 +33,6 @@ export default function App() {
               }}
             />
           </Stack.Navigator>
-          {/* <HomeScreen /> */}
         </SafeAreaProvider>
       </NavigationContainer>      
     </Provider>
