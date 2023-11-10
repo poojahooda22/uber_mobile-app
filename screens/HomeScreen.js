2bed0bd4e74faa997f89ea4bce5394798b49b87e
pooja <91055527+poojahooda22@users.noreply.github.com>
2023-11-10 17:08:01 +0530

    updated homeScreen

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View, SafeAreaView, Image } from 'react-native'
+import { StyleSheet, View, SafeAreaView, Image } from 'react-native'
 import React from 'react';
 import tw from 'twrnc';
 import NavOptions from '../components/NavOptions';
