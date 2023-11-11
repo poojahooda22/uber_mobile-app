d3a1036ff9c991bcf49da3bb5402e648d207a433
pooja <91055527+poojahooda22@users.noreply.github.com>
2023-11-11 14:36:08 +0530

    updated homescreen

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -2,6 +2,9 @@ import { StyleSheet, View, SafeAreaView, Image } from 'react-native'
 import React from 'react';
 import tw from 'twrnc';
 import NavOptions from '../components/NavOptions';
+import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
+import { GOOGLE_MAPS_APIKEY } from '@env';
+
 
 const HomeScreen = () => {
   return (
@@ -16,6 +19,25 @@ const HomeScreen = () => {
           source={{
             uri: "https://links.papareact.com/gzs"
           }}
+        />
+
+        <GooglePlacesAutocomplete
+          placeholder="Search for location"
+          styles={{
+            container: {
+              flex: 0,
+            },
+            textInput: {
+              fontSize: 18,
+            }
+          }}
+          minLength={2}
+          query={{
+            key: GOOGLE_MAPS_APIKEY,
+            language: 'en',
+          }} 
+          nearbyPlacesAPI="GooglePlacesSearch"
+          debouce={400}
         />
         <NavOptions />
       </View>
