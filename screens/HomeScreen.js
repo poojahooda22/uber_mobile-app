ea8c0987d12a4f5a1de711996f5f801e2435ea52
pooja <91055527+poojahooda22@users.noreply.github.com>
2023-11-11 15:19:54 +0530

    updated homescreen

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -4,9 +4,13 @@ import tw from 'twrnc';
 import NavOptions from '../components/NavOptions';
 import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
 import { GOOGLE_MAPS_APIKEY } from '@env';
+import { useDispatch } from 'react-redux';
+import { setDestination, setOrigin } from '../slices/navSlice';
 
 
 const HomeScreen = () => {
+
+  const dispatch = useDispatch();
   return (
     <SafeAreaView style={tw `bg-white h-full`}>
       <View style={tw `p-5`}>
@@ -31,6 +35,18 @@ const HomeScreen = () => {
               fontSize: 18,
             }
           }}
+          onPress={(data, details = null) => {
+            dispatch(
+              setOrigin({
+              location: details.geometry.location,
+              description: data.description,
+            })
+            );
+            dispatch(setDestination(null));
+          }}
+          fetchDetails={true}
+          returnKeyType={"search"}
+          enablePoweredByContainer={false}
           minLength={2}
           query={{
             key: GOOGLE_MAPS_APIKEY,
