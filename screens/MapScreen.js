8403041741fd7efc0b1025384556ec7c737cbe58
pooja <91055527+poojahooda22@users.noreply.github.com>
2023-11-11 15:20:07 +0530

    updated mapscreen

--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -1,10 +1,20 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import Map from '../components/Map'
+import tw from 'twrnc';
 
 const MapScreen = () => {
   return (
     <View>
-      <Text>Here are the maps</Text>
+      <View
+        style={tw `h-1/2`}
+      >
+        <Map />
+      </View>
+
+      <View style={tw `h-1/2`}>
+
+      </View>
     </View>
   )
 }
