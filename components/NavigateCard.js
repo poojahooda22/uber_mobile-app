91b8229e5b964ba7201a6033578851c052ae9a61
pooja <91055527+poojahooda22@users.noreply.github.com>
2023-11-12 13:15:49 +0530

    updated navigation

--- components/NavigateCard.js
+++ components/NavigateCard.js
@@ -1,14 +1,61 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View, SafeAreaView } from 'react-native'
+import React from 'react';
+import { useDispatch } from 'react-redux';
+import tw from 'twrnc';
+import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
+import { GOOGLE_MAPS_APIKEY } from '@env';
+import { setDestination } from '../slices/navSlice';
 
 const NavigateCard = () => {
+    const dispatch = useDispatch();
   return (
-    <View>
-      <Text>NavigateCard</Text>
-    </View>
+    <SafeAreaView style={tw`bg-white flex-1`}>
+      <Text style={tw `text-center py-4 text-xl`}>Good Morning, Pooja</Text>
+      <View style={tw `border-t border-gray-200 flex-shrink`}>
+        <View>
+            <GooglePlacesAutocomplete
+                placeholder="Where to?"
+                styles={toInputBoxStyles}
+                fetchDetails={true}
+                returnKeyType={"search"}
+                minLength={2}
+                onPress={(data, details = null) => {
+                    dispatch(
+                        setDestination({
+                            location: details.geometry.location,
+                            description: data.description,
+                        })
+                    );
+                }}
+                query={{
+                key: GOOGLE_MAPS_APIKEY,
+                language: 'en',
+                }}
+                nearbyPlacesAPI="GooglePlacesSearch"
+                debounce={400}
+            />
+        </View>
+
+      </View>
+    </SafeAreaView>
   )
 }
 
 export default NavigateCard
 
-const styles = StyleSheet.create({})
+const toInputBoxStyles = StyleSheet.create({
+    container: {
+        background: "white",
+        paddingTop: 20,
+        flex: 0,
+    },
+    textInput: {
+        backgroundColor: "#DDDDDF",
+        borderRadius: 8,
+        fontSize: 18,
+    },
+    textInputContainer: {
+        paddingHorizontal: 20,
+        paddingBottom: 0,
+    }
+})
