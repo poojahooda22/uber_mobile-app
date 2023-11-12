b2a6b7270be47ea80a79c0fbb8c78857d26d3e42
pooja <91055527+poojahooda22@users.noreply.github.com>
2023-11-12 11:49:57 +0530

    updated map

--- components/Map.js
+++ components/Map.js
@@ -1,33 +1,50 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 import MapView, { Marker } from 'react-native-maps';
+import { useSelector } from 'react-redux';
 import tw from 'twrnc';
+import { selectOrigin } from '../slices/navSlice';
 
 
 const Map = () => {
+
+    const origin = useSelector(selectOrigin);
   return (
-    <MapView
-        style={tw `flex-1`}
-        mapType='mutedStandard'
-        initialRegion={{
-            latitude: 37.78825,
-            longitude: -122.4324,
-            latitudeDelta: 0.0922,
-            longitudeDelta: 0.0421,
-        }}
-    >
-        {origin?.location && (
-            <Marker
-                coordinate={
-                    {
-                        latitude: origin.location.lat,
-                        longitude: origin.location.lng,
+    // <MapView
+    //     style={tw `flex-1`}
+    //     mapType='mutedStandard'
+    //     initialRegion={{
+    //         latitude: origin.location.lat,
+    //         longitude: origin.location.lng,
+    //         latitudeDelta: 0.005,
+    //         longitudeDelta: 0.005,
+    //     }}
+    // >
+    //     {origin?.location && (
+    //         <Marker
+    //             coordinate={
+    //                 {
+    //                     latitude: origin.location.lat,
+    //                     longitude: origin.location.lng,
                     
-                    }
-                }
-            />
-        )}
-    </MapView>
+    //                 }
+    //             }
+    //             title="Origin"
+    //             description={origin.description}
+    //             identifier="origin"
+    //         />
+    //     )}
+    // </MapView>
+    <MapView
+      style={tw`flex-1`}
+      mapType="mutedStandard"
+      initialRegion={{
+        latitude: 37.78825,
+        longitude: -122.4324,
+        latitudeDelta: 0.0922,
+        longitudeDelta: 0.0421,
+      }}
+      />
   )
 }
 
