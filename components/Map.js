a9896aca918dff6be2ab6660265b084d88fe7fa3
pooja <91055527+poojahooda22@users.noreply.github.com>
2023-11-12 14:05:19 +0530

    updated

--- components/Map.js
+++ components/Map.js
@@ -10,41 +10,31 @@ const Map = () => {
 
     const origin = useSelector(selectOrigin);
   return (
-    // <MapView
-    //     style={tw `flex-1`}
-    //     mapType='mutedStandard'
-    //     initialRegion={{
-    //         latitude: origin.location.lat,
-    //         longitude: origin.location.lng,
-    //         latitudeDelta: 0.005,
-    //         longitudeDelta: 0.005,
-    //     }}
-    // >
-    //     {origin?.location && (
-    //         <Marker
-    //             coordinate={
-    //                 {
-    //                     latitude: origin.location.lat,
-    //                     longitude: origin.location.lng,
-                    
-    //                 }
-    //             }
-    //             title="Origin"
-    //             description={origin.description}
-    //             identifier="origin"
-    //         />
-    //     )}
-    // </MapView>
     <MapView
-      style={tw`flex-1`}
-      mapType="mutedStandard"
-      initialRegion={{
-        latitude: 37.78825,
-        longitude: -122.4324,
-        latitudeDelta: 0.0922,
-        longitudeDelta: 0.0421,
-      }}
-      />
+        style={tw `flex-1`}
+        mapType='mutedStandard'
+        initialRegion={{
+            latitude: origin.location.lat,
+            longitude: origin.location.lng,
+            latitudeDelta: 0.005,
+            longitudeDelta: 0.005,
+        }}
+    >
+        {origin?.location && (
+            <Marker
+                coordinate={
+                    {
+                        latitude: origin.location.lat,
+                        longitude: origin.location.lng,
+                    
+                    }
+                }
+                title="Origin"
+                description={origin.description}
+                identifier="origin"
+            />
+        )}
+    </MapView>
   )
 }
 
