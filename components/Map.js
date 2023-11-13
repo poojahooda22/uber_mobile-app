10599da530a931410e78d14c26bcee5750c9c036
pooja <91055527+poojahooda22@users.noreply.github.com>
2023-11-13 11:21:18 +0530

    updated

--- components/Map.js
+++ components/Map.js
@@ -22,7 +22,8 @@ const Map = () => {
         mapRef.current.fitToSuppliedMarkers(['origin', 'destination'], {
             edgePadding: { top: 50, right: 50, bottom: 50, left: 50 },
         }); 
-    }, [origin, destination])
+    }, [origin, destination]);
+
   return (
     <MapView
         style={tw `flex-1`}
