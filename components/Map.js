97c969dbf11c6bc112ad499e03265da685044d4e
pooja <91055527+poojahooda22@users.noreply.github.com>
2023-11-13 11:52:15 +0530

    updated

--- components/Map.js
+++ components/Map.js
@@ -19,15 +19,16 @@ const Map = () => {
         if (!origin || !destination) return;
 
         // Zoom & fit to markers
-        mapRef.current.fitToSuppliedMarkers(['origin', 'destination'], {
+        mapRef.current.fitToSuppliedMarkers([ "origin", "destination"], {
             edgePadding: { top: 50, right: 50, bottom: 50, left: 50 },
         }); 
     }, [origin, destination]);
 
   return (
     <MapView
+        ref={mapRef}
         style={tw `flex-1`}
-        mapType='mutedStandard'
+        mapType="mutedStandard"
         initialRegion={{
             latitude: origin.location.lat,
             longitude: origin.location.lng,
