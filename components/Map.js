cb4f5feaedbd743d074e627f7da7c2918ccb483a
pooja <91055527+poojahooda22@users.noreply.github.com>
2023-11-13 09:20:24 +0530

    updated

--- components/Map.js
+++ components/Map.js
@@ -10,7 +10,7 @@ import { GOOGLE_MAPS_APIKEY } from '@env';
 const Map = () => {
 
     const origin = useSelector(selectOrigin);
-    // const destination = useSelector(selectDestination);
+    const destination = useSelector(selectDestination);
 
 
   return (
