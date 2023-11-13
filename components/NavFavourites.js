f178a5a50f0bc10443f8a015a1602d95e5057cdc
pooja <91055527+poojahooda22@users.noreply.github.com>
2023-11-13 12:05:09 +0530

    updated

--- components/NavFavourites.js
+++ components/NavFavourites.js
@@ -1,5 +1,8 @@
 import { StyleSheet, Text, View, FlatList, TouchableOpacity } from 'react-native'
 import React from 'react'
+import tw from 'twrnc';
+import { Icon } from 'react-native-elements';
+
 
 const data = [
     { 
@@ -22,9 +25,15 @@ const NavFavourites = () => {
     <FlatList 
         data={data}
         keyExtractor={(item) => item.id}
-        renderItem={({item}) =>(
+        renderItem={({item: { location, destination, icon }}) =>(
             <TouchableOpacity>
-
+                <Icon
+                    style={tw `mr-4 rounded-full bg-gray-300 p-3`}
+                    name={icon}
+                    type="ionicon"
+                    color="white"
+                    size={18}
+                />
             </TouchableOpacity>
         )}
     >
