f750e5ae9d4b41dfe7f9165a5edc1c09f4c9ccc8
pooja <91055527+poojahooda22@users.noreply.github.com>
2023-11-13 12:02:15 +0530

    updated

--- components/NavFavourites.js
+++ components/NavFavourites.js
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View, FlatList } from 'react-native'
+import { StyleSheet, Text, View, FlatList, TouchableOpacity } from 'react-native'
 import React from 'react'
 
 const data = [
